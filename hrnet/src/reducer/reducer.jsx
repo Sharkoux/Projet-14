7b8cb46d4033b2b90fa211c88798c2f822b354ce
Jean-Baptiste Guerin <jb.l.guerin@gmail.com>
2023-04-03 18:03:28 +0200

    feat: init redux

---
+++ hrnet/src/reducer/reducer.jsx
@@ -0,0 +1,19 @@
+
+// import actionType
+import { Types } from '../actions/actionTypes';
+
+
+
+
+
+// init state
+const initialState = {
+
+}
+
+// Use initialState by Default
+const reducer_user = (state = initialState, action) => {
+
+}
+
+export default reducer_user
